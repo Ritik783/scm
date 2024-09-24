f32dfbffdf54c3c2107081a249b2b1646ae0c2b3
Ritik kumar <ritikkumardhindar@gmail.com>
2024-09-24 22:49:54 +0530

    by ritik

--- src/main/java/com/scm/Controller/PageController.java
+++ src/main/java/com/scm/Controller/PageController.java
@@ -3,18 +3,21 @@ package com.scm.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PageController {
     @RequestMapping("/home")
     public String homeController(Model model){
         System.out.println("this is the home");
-        model.addAttribute("name","Technology");
-        model.addAttribute("Github","https://github.com/Ritik783/CodeClause_Product_landing_page");
-        model.addAttribute("Leetcode","https://leetcode.com/u/ritik78/");
+        // model.addAttribute("name","Technology");
+        // model.addAttribute("Github","https://github.com/Ritik783/CodeClause_Product_landing_page");
+        // model.addAttribute("Leetcode","https://leetcode.com/u/ritik78/");
         return "home";
     }
-    // about route
+    // about 
     @RequestMapping("/about")
     public String aboutPagelayout(Model model){
         model.addAttribute("isLogin",false);
@@ -24,8 +27,25 @@ public class PageController {
     // about service
     @RequestMapping("/services")
     public String servicePage(){
-        return "aboutservice";
+        return "services";
     }
+
+    @RequestMapping("/contact")
+    public String contactPage(Model model) {
+        return "Contact";
+    }
+    
+    @RequestMapping("/login")
+    public String loginPage(Model model) {
+        return "Login";
+    }
+
+    @RequestMapping("/signup")
+    public String signupPage(Model model) {
+        return "Signup";
+    }
+    
+    
 
 
 }
